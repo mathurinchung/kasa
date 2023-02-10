9496a36b0ff9d31b9ef4a32ed5910e7fdb424e82
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-02-10 00:18:51 +0000

    Update Footer

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -1,6 +1,14 @@
+import Logo from '../assets/svg/white_logo.svg';
+import '../css/Footer.css';
+
 function Footer() {
   return (
-    <footer id="Footer"></footer>
+    <footer id="Footer">
+      <div className="wrapper">
+        <img src={ Logo } alt="logo" />
+        <p>&copy; 2020 Kasa. All rights reserved</p>
+      </div>
+    </footer>
   );
 }
 
