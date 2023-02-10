5e185b0ac37f2adb05cb7315ae068baffdc5aacf
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-02-10 00:40:08 +0000

    Update NotFound

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Router />
+      <main id="Main">
+        <Router />
+      </main>
       <Footer />
     </div>
   );
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+import '../css/NotFound.css';
+
 function NotFound() {
   return (
-    <div id="NotFound"></div>
+    <div id="NotFound">
+      <p className="errorCode">404</p>
+      <p className="errorMsg">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </div>
   );
 }
 
