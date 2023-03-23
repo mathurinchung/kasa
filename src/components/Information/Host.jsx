c54592fda37062e21766441228f6d9e3b48b9cd8
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-03-23 01:32:11 +0000

    update hostname min-content

--- src/components/Information/Host.jsx
+++ src/components/Information/Host.jsx
@@ -1,10 +1,7 @@
 function Host({ data }) {
   return (
     <div className="host">
-      <h3 className="hostname">
-        { data.host.name.split(" ")[0] } <br />
-        { data.host.name.split(" ")[1] }
-      </h3>
+      <h3 className="hostname">{ data.host.name }</h3>
       <div className="profile">
         <img src={ data.host.picture } alt={ data.title } />
       </div>
